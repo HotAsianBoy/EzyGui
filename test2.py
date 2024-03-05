d5c62addd49790395ddff2ff676071d67a3b8ace
HotAsianBoy <wongg@middleton.school.nz>
2024-03-06 09:13:28 +1300

    test2
    easygui test 2

---
+++ test2.py
@@ -0,0 +1,9 @@
+word = "fantastic"
+letters = list(word)
+print(letters)
+
+fruit = ["Apples", "Pears", "Bananas", "Mandarins", "Plums"]
+# fruit_list = fruit.split()
+
+fruit_list = fruit.split()
+print(fruit_list)
