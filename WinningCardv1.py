a9090492dc4368de98e8229c717ec3d43c8b3789
HotAsianBoy <wongg@middleton.school.nz>
2024-03-10 15:14:32 +1300

    completed (error)
    finished however got errors

---
+++ WinningCardv1.py
@@ -0,0 +1,30 @@
+import easygui
+import random
+
+
+card_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+card_suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
+while True:
+    player = [random.choice(card_list), random.choice(card_suits)]
+    computer = [random.choice(card_list), random.choice(card_suits)]
+    if player == computer:
+        continue
+    elif card_list.index(player[0]) == card_list.index(computer[0]):
+        result = "The game is a draw!"
+    elif card_list.index(player[0]) > card_list.index(computer[0]):
+        result = "Nice try, you lost!"
+    else:
+        result = "Congratulations! You won!"
+    play_again = easygui.enterbox(f"You, have the {player[0]} of "
+                                    f" {player[1]}\n, and I have the "
+                                    f"{computer[0]} of {computer[1]}!\n\n"
+                                    f"******{result}******\n\n"
+                                    f"Do you want to play again?",
+                                    "Game result", choices=["Yes","No"])
+
+    if play_again == "No":
+        easygui.msgbox("Thank you for playing! Have a good day!")
+    break
+
+
+
